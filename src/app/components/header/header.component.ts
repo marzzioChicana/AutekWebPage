374f6a53f32f0211ac4ad6e98b691f14601c5fad
Marzzio Chicana Romero <89101069+marzzioChicana@users.noreply.github.com>
2024-02-12 17:22:15 -0500

    mejoras v3

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   logoPath: string = 'assets/logo-autek.png';
+
+  activeButtonId: string | null = null;
+
+  setActive(id: string) {
+    this.activeButtonId = id;
+    this.setInactiveOpacity();
+  }
+
+  clearActive() {
+    this.activeButtonId = null;
+    this.setNormalOpacity();
+  }
+
+  private setInactiveOpacity() {
+    const buttons = document.querySelectorAll('.toolbar-button');
+    buttons.forEach((button: Element) => { // Cambiamos HTMLElement a Element
+      if ((button as HTMLElement).id !== this.activeButtonId) {
+        (button as HTMLElement).classList.add('inactive');
+      }
+    });
+  }
+  
+  private setNormalOpacity() {
+    const buttons = document.querySelectorAll('.toolbar-button');
+    buttons.forEach((button: Element) => { // Cambiamos HTMLElement a Element
+      (button as HTMLElement).classList.remove('inactive');
+    });
+  }
 }
